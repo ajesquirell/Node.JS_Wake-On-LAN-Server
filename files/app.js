c5116d67cc4739c0439e35d7555e38a5fa2eed46
Austin Esquirell <47311328+ajesquirell@users.noreply.github.com>
2021-05-31 22:01:17 -0400

    Add files via upload

---
+++ files/app.js
@@ -0,0 +1,29 @@
+var deviceData;
+
+const fetchJson = async () => {
+    const response = await fetch('macs.json');
+
+    if (response.status !== 200)
+        throw new Error('Could not fetch devices');
+
+    const data = await response.json();
+    return data;
+}
+
+fetchJson()
+    .then(data =>{
+        console.log('data: ', data);
+        deviceData = data
+
+        for (const p in deviceData) {
+            var listItem = document.createElement("li");
+            var text = document.createTextNode(deviceData[p]["name"]);
+            console.log(deviceData[p]["name"]);
+            listItem.appendChild(text);
+            document.getElementById('devices').appendChild(listItem);
+            listItem.onclick = () => {window.location.href = `/wake_device?mac_address=${deviceData[p]["mac"]}`};
+        }
+    })
+    .catch(err => console.log('error: ', err));
+
+
